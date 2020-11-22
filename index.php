fe9eb1b343d1ca03a18f4efe081fc3bbac2b9198
Natsu-dev <Natsu-aoiro@s.okayama-u.ac.jp>
2020-11-22 23:28:27 +0900

    index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php
-    echo "Hello";
-    echo $_GET['number'];
+<?php if(isset($_GET['switch'])) { $switch = $_GET['switch']; } // URL中のswitchパラメータを取得
+    //$pdo = new PDO('mysql:host=localhost;dbname=remotepc;charset=utf8','default','test');
+    //$qry = $pdo->prepare('SELECT isRunning FROM current');
+    //$qry->execute();
+    //$a = $qry->fetchAll();
+    //print_r($a[0]['isRunning']);
+
+    $pdo = new PDO('mysql:host=localhost;dbname=remotepc;charset=utf8','default','test');
+    $qry = $pdo->prepare('UPDATE current SET isRunning = 1');
+    $qry->execute();
+    //$a = $qry->fetchAll();
+    //print_r($a[0]['isRunning']);
 ?>
