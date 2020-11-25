25fe1931a810e913d375937c09d2980a37f89b55
Natsu-dev <Natsu-aoiro@s.okayama-u.ac.jp>
2020-11-25 16:35:01 +0900

    [update] とりあえず動いたので使わない表示を削除

--- index.php
+++ index.php
@@ -15,8 +15,6 @@ date_default_timezone_set('Asia/Tokyo'); //タイムゾーンを東京に設定
         $query = $pdo->prepare('UPDATE current SET isRunning = :changed'); //$changedの値をisRunningにセットする準備
         $query->bindValue(':changed', (int)$changed, PDO::PARAM_INT); //$changedの値をisRunningの?部分にバインド
         $query->execute(); //実行
-        echo '<p>update done</p>';
-        echo '<p>' + $nowtime + '</p>';
 
         $query2 = $pdo->prepare('INSERT INTO changed_log (changed, changed_date, changed_ip) VALUES (:changed, :changed_date, :changed_ip)');
 
@@ -25,22 +23,9 @@ date_default_timezone_set('Asia/Tokyo'); //タイムゾーンを東京に設定
         $query2->bindValue(':changed_date', $nowtime);
         $query2->bindValue(':changed_ip', $_SERVER["REMOTE_ADDR"]);
         $query2->execute(); //実行
-        echo '<p>record done</p>';
+        
     } catch (PDOException $ex) {
 
         exit('<p>データベース反映失敗</p>' . $ex->getMessage());
     }
 }
-?>
-
-//$pdo = new PDO('mysql:host=localhost;dbname=remotepc;charset=utf8','default','test');
-//$query = $pdo->prepare('SELECT isRunning FROM current');
-//$query->execute();
-//$a = $query->fetchAll();
-//print_r($a[0]['isRunning']);
-
-$pdo = new PDO('mysql:host=localhost;dbname=remotepc;charset=utf8','default','test');
-$query = $pdo->prepare('UPDATE current SET isRunning = 1');
-$query->execute();
-//$a = $query->fetchAll();
-//print_r($a[0]['isRunning']);
