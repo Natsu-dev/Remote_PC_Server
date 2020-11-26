88631f0611532f2aa9496111b1fff19f9d38f938
Natsu-dev <Natsu-aoiro@s.okayama-u.ac.jp>
2020-11-26 19:25:28 +0900

    サーバー仮完成

--- index.php
+++ index.php
@@ -1,31 +1,48 @@
 <?php
-date_default_timezone_set('Asia/Tokyo'); //タイムゾーンを東京に設定
-?>
+    date_default_timezone_set('Asia/Tokyo'); //タイムゾーンを東京に設定
+    $nowtime = date('Y-m-d H:i:s'); //実行される時刻を記録 (string型?)
+    $pdo = new PDO('mysql:host=localhost;dbname=remotepc;charset=utf8', 'default', 'test'); //PDOを宣言
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //例外用
 
-<?php if (isset($_GET['changed'])) { // URLにupdateパラメータがある → スイッチ更新処理
+    if (isset($_GET['changed'])) { //URLにchangedパラメータがある → スイッチ更新処理
 
-    $nowtime = date("Y-m-d H:i:s"); //実行される時刻を記録 (string型?)
+        $changed = $_GET['changed']; //changedの値を取得
 
-    $changed = $_GET['changed']; //updateの値を取得
-    $pdo = new PDO('mysql:host=localhost;dbname=remotepc;charset=utf8', 'default', 'test'); //Update用のPDOを宣言
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //例外用
+        try {
 
-    try {
+            $query = $pdo -> prepare('UPDATE current SET isRunning = :changed'); //changedを更新する準備
+            $query -> bindValue(':changed', (int)$changed, PDO::PARAM_INT); //$changedの値をバインド
+            $query -> execute(); //実行
 
-        $query = $pdo->prepare('UPDATE current SET isRunning = :changed'); //$changedの値をisRunningにセットする準備
-        $query->bindValue(':changed', (int)$changed, PDO::PARAM_INT); //$changedの値をisRunningの?部分にバインド
-        $query->execute(); //実行
-
-        $query2 = $pdo->prepare('INSERT INTO changed_log (changed, changed_date, changed_ip) VALUES (:changed, :changed_date, :changed_ip)');
-
-        //$query2->bindValue(':id', null, PDO::PARAM_INT);
-        $query2->bindValue(':changed', (int)$changed, PDO::PARAM_INT);
-        $query2->bindValue(':changed_date', $nowtime);
-        $query2->bindValue(':changed_ip', $_SERVER["REMOTE_ADDR"]);
-        $query2->execute(); //実行
+            $query2 = $pdo -> prepare('INSERT INTO changed_log (changed, changed_date, changed_ip) VALUES (:changed, :changed_date, :changed_ip)'); //changed_log追加の準備
+            $query2 -> bindValue(':changed', (int)$changed, PDO::PARAM_INT); //$changedの値をバインド
+            $query2 -> bindValue(':changed_date', $nowtime); //$nowtimeの値をバインド
+            $query2 -> bindValue(':changed_ip', $_SERVER["REMOTE_ADDR"]); //IPアドレスの値をバインド
+            $query2 -> execute(); //実行
         
-    } catch (PDOException $ex) {
+        } catch (PDOException $ex) { //PDO例外をキャッチ
 
-        exit('<p>データベース反映失敗</p>' . $ex->getMessage());
+            exit('<p>データベース反映失敗</p>' . $ex -> getMessage());
+
+        }
     }
-}
+
+    if (isset($_GET['reader'])) { //URLにreaderパラメータがある → スイッチ確認処理
+
+        try {
+
+            $query = $pdo -> prepare('UPDATE raspi_refresh SET last_access = :last_access'); //$changedの値をisRunningにセットする準備
+            $query -> bindValue(':last_access', $nowtime); //$changedの値をisRunningの?部分にバインド
+            $query -> execute(); //実行
+
+            $state = $pdo -> prepare('SELECT isRunning FROM current'); //isRunningの値を取得する準備
+            $state -> execute(); //実行
+            $isRun = $state -> fetch(PDO::FETCH_ASSOC); //得られたステートメントから値をフェッチ
+            print_r($isRun['isRunning']); //フェッチした$isRunの値をエコー
+        
+        } catch (PDOException $ex) { //PDO例外をキャッチ
+
+            exit('-1' . $ex -> getMessage()); //$isRunの代わりに'-1'を返す
+        
+        }
+    }
